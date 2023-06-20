2e41b36a83bc1bf8f6ae179d317a49b36b9e0e2e
Егор <saleroses@yandex.ru>
2023-06-20 16:30:44 +0300

    first

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -8,7 +8,8 @@ const Main = () => {
             <div className={s.foto}>
                 <div className={s.container}>
                     <h1>Привет! Я Егор</h1>
-                    <span className={s.text}>Frontend developer</span>
+                    <span className={s.text}>Frontend developer located in Sait-P</span>
+                    <p className={s.text2}>Specialized in <span className={s.specialized}>Creating Websites</span></p>
                 </div>
             </div>
 
